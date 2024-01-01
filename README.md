4b7b337c990373f3c3d56e461464457431869389
Mateusz Horczak <82621@student.pb.edu.pl>
2024-01-01 16:07:28 +0100

    make readme and last fixes

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Scrabble Game Repository
+
+Welcome to the Scrabble Game repository! This project is an implementation of the classic Scrabble game in Python. Below, you'll find instructions on how to run the game along with some visual insights from the gameplay.
+
+## About
+
+This repository contains the source code for a Scrabble game implemented in Python. The game provides both a Text-based User Interface (TUI) and a Graphical User Interface (GUI) for a versatile gaming experience.
+
 # How to run?
 1. Clone repo
 `git clone git@github.com:mateuszhorczak/KCK_Scrabble.git`
@@ -9,6 +17,29 @@
 `python main.py`
 5. if you want run in dev mode, you can do it with
 `textual run --dev main.py`
+
+## Gameplay Preview
+
+Here are some glimpses of the Scrabble game:
+
+1. ![Screenshot 1](assets/choose_user_interface.png)
+
+   *Select user interface.*
+
+2. ![Screenshot 2](assets/enter_user_nickanames_GUI.png)
+   *Starting the game on the gui.*
+
+3. ![Screenshot 3](assets/game_board_TUI.png)
+   *Game board on the TUI.*
+
+4. ![Screenshot 4](assets/game_board_GUI.png)
+   *Game board on the GUI*
+
+5. ![Screenshot 5](assets/end_game_TUI.png)
+   *End game on the TUI.*
+
+
+Feel free to explore the game and enjoy playing Scrabble!
 
 ## Authors
 
--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 from view.gui_view import ScrabbleApp as appGUI
 from view.tui_view import ScrabbleApp as appTUI
 
-from tkinter import *
-from tkinter import ttk
-import tkinter as tk
+try:
+    import tkinter as tk
+    from tkinter import ttk
+except ImportError:
+    tk = None
 
-tui = False
 
-if __name__ == "__main__":
-    if tui:
-        app = appTUI()
-        app.run()
-    else:
+def start_application(use_gui):
+    if use_gui:
         app = appGUI()
         app.mainloop()
+    else:
+        app = appTUI()
+        app.run()
+
+
+def run_with_gui():
+    root = tk.Tk()
+    root.title("Scrabble Game - Wybór interfejsu")
+    root.geometry("400x200")
+
+    style = ttk.Style()
+    style.configure("TButton", padding=10, font=("Helvetica", 12), background="darkolivegreen2",
+                    foreground="darkolivegreen4")
+
+    def start_gui():
+        root.destroy()
+        start_application(True)
+
+    def start_tui():
+        root.destroy()
+        start_application(False)
+
+    ttk.Button(root, text="Uruchom interfejs tekstowy", command=start_tui, style="TButton").pack(pady=10)
+    ttk.Button(root, text="Uruchom interfejs graficzny", command=start_gui, style="TButton").pack(pady=10)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    if tk:
+        run_with_gui()
+    else:
+        print("Biblioteka tkinter nie jest dostępna. Uruchamiam interfejs tekstowy.")
+        start_application(False)
